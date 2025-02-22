4ca2f293abd5a9d2a264ddbfa5958b8a6ef90d78
Walmyr <wlsf82@gmail.com>
2025-02-22 01:20:37 +0100

    WIP (Draft Cypress API tests)
    Figure out why GUI tests pass in open mode and fail in run mode.

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,9 @@ const tasks = require('./cypress/tasks')
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    fixturesFolder: false,
+    env: {
+      API_URL: 'http://localhost:3001/api'
+    },
     supportFile: false,
     setupNodeEvents(on, config) {
       tasks(on)
